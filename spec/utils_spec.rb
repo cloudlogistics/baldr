f8f77b7465fe98dbc435ca5ed9b5998162343b67
npersad84@gmail.com <npersad84@gmail.com>
2016-07-26 17:07:29 -0400

    Even Better :).

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -2,41 +2,11 @@ require 'spec_helper'
 
 describe Baldr::Utils do
 
-  # Dir.glob('spec/support/edi_files/valid/210/2.EDI').each do |file|
-  #   context file do
-  #     it 'should dump tree to json and then load it' do
-  #       input = File.read(file)
-  #       parser = Baldr::Parser.new(input)
-  #       parser.error.should be_nil
-
-  #       envelopes1 = parser.envelopes
-  #       dump = Baldr::Utils.dump(envelopes1)
-
-  #       envelopes2 = Baldr::Utils.load(dump)
-  #       envelopes2.each do |envelope|
-  #         Baldr::Types.convert_after_load!(envelope, parser.grammar)
-  #       end
-  #       envelopes1.each do |envelope|
-  #         Baldr::Types.convert_before_render!(envelope, parser.grammar)
-  #       end
-  #       envelopes2.each do |envelope|
-  #         Baldr::Types.convert_before_render!(envelope, parser.grammar)
-  #       end
-  #       output1 = Baldr::Renderer::X12.draw(envelopes1, {separators: parser.separators})
-  #       output1.bytes.to_a.should eq input.bytes.to_a
-
-  #       output2 = Baldr::Renderer::X12.draw(envelopes2, {separators: parser.separators})
-  #       output1.bytes.to_a.should eq output2.bytes.to_a
-  #     end
-  #   end
-  # end
-
-  Dir.glob('spec/support/edi_files/version_4060/**/*.EDI').each do |file|
+  Dir.glob('spec/support/edi_files/valid/**/*.EDI').each do |file|
     context file do
       it 'should dump tree to json and then load it' do
         input = File.read(file)
         parser = Baldr::Parser.new(input)
-
         parser.error.should be_nil
 
         envelopes1 = parser.envelopes
@@ -60,5 +30,5 @@ describe Baldr::Utils do
       end
     end
   end
-
+  
 end
